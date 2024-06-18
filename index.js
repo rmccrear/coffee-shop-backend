677ec2fc2100f27826701e48840fd4137b37478c
Lewis Benson <tm.lewis.benson@gmail.com>
2024-06-17 20:24:35 -0400

    add notFound middleware to routes

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const cors = require('cors');
 const baseRouter = require('./src/routes/baseRoutes');
 const { error } = require('./src/middleware/error');
 const mongooseConnect = require('./database');
+const notFound = require('./src/middleware/notFound404');
 app.use(express.json());
 app.use(cors());
 app.use(baseRouter);
 
+app.use(notFound);
 mongooseConnect().catch((err) => console.error(err));
 app.use(error);
 app.listen(PORT, () => {
