3cef197c0736cc30b2b6f850529e5c5d607a70f0
Lewis Benson <tm.lewis.benson@gmail.com>
2024-06-17 21:27:31 -0400

    add product route

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const baseRouter = require('./src/routes/baseRoutes');
 const { error } = require('./src/middleware/error');
 const mongooseConnect = require('./database');
 const notFound = require('./src/middleware/notFound404');
+const productRouter = require('./src/routes/productRoutes');
 app.use(express.json());
 app.use(cors());
 app.use(baseRouter);
-
+app.use('/product', productRouter);
 app.use(notFound);
 mongooseConnect().catch((err) => console.error(err));
 app.use(error);
+
 app.listen(PORT, () => {
   console.log('Server running on port: ' + PORT);
 });
