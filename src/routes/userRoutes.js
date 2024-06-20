c8b9d356aec265a9915b595f6ff7e8bdf2527d34
Lewis Benson <tm.lewis.benson@gmail.com>
2024-06-19 21:05:44 -0400

    added CRUD routes for users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,11 @@
-const { register, login, getUsers } = require('../controllers/userControllers');
+const {
+  register,
+  login,
+  getUsers,
+  deleteOneUser,
+  getOneUser,
+  updateOneUser,
+} = require('../controllers/userControllers');
 const auth = require('../middleware/auth');
 const { checkEmail, checkPassword, signToken } = require('../middleware/login');
 const role = require('../middleware/role');
@@ -10,5 +17,11 @@ userRouter.route('/register').post(register);
 userRouter.route('/login').post(checkEmail, checkPassword, signToken, login);
 
 userRouter.route('/').get(auth, role(['admin']), getUsers);
+
+userRouter
+  .route('/:userId')
+  .get(auth, role(['user', 'admin']), getOneUser)
+  .delete(auth, role(['admin']), deleteOneUser)
+  .put(auth, role(['user', 'admin']), updateOneUser);
 
 module.exports = userRouter;
