d05f9b2ce34db9c6101da6bfd0aa2b7bf00bed12
Lewis Benson <tm.lewis.benson@gmail.com>
2024-06-19 21:05:22 -0400

    create user controllers

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -28,4 +28,72 @@ async function getUsers(req, res, next) {
   }
 }
 
-module.exports = { login, register, getUsers };
+async function getOneUser(req, res, next) {
+  const { userId } = req.params;
+  try {
+    if (req.role === 'user') {
+      if (req.userId === userId) {
+        const user = await User.findById(userId);
+        return res.status(200).json(user);
+      }
+    } else if (req.role === 'admin') {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found!' });
+      }
+      return res.status(200).json(user);
+    }
+
+    res.status(403).json({ error: ' Not Allowed.' });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateOneUser(req, res, next) {
+  try {
+    const { userId } = req.params;
+    console.log(userId);
+    const { name, email, password, role } = req.body;
+    // const user = await User.findByIdAndUpdate(userId ,req.body, {new: true})
+    const user = await User.findById(userId);
+    console.log(user, '-----------------');
+    if (!user) {
+      return res.status(404).json({ error: 'User not found!' });
+    }
+    if (req.role === 'admin') {
+      if (role) user.role = role;
+    }
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (password) user.password = password;
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteOneUser(req, res, next) {
+  try {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+    res.status(204).json({ message: 'deleted' });
+  } catch (error) {
+    if (error.name === 'CastError') {
+      return res.status(400).json({ error: 'Invalid ID provided.' });
+    }
+    next(error);
+  }
+}
+
+module.exports = {
+  login,
+  register,
+  getUsers,
+  deleteOneUser,
+  updateOneUser,
+  getOneUser,
+};
