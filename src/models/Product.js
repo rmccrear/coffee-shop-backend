22e87e38a92995489951b3f2fb4945008ff71fb9
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-12-24 12:14:00 -0800

    make some product fields optional

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,8 +5,8 @@ const productSchema = new mongoose.Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
-  stock: { type: Number, required: true },
-  imageUrl: { type: String, required: true },
+  stock: { type: Number  },
+  imageUrl: { type: String },
 });
 
 const Product = mongoose.model('Product', productSchema);
