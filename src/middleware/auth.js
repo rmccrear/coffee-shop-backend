10b375405e2354edac66da7b4a5bdd3c8c114b48
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-01-27 12:33:20 -0800

    add error checking for missing auth header

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,6 +2,9 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
+  if(!req.header('Authorization')) {
+    return res.status(401).json({ error: 'Access denied. Provide a token.' });
+  }
   const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) {
     return res.status(401).json({ error: 'Access denied. Provide a token.' });
@@ -13,6 +16,7 @@ function auth(req, res, next) {
     req.user = decoded;
     next();
   } catch (error) {
+    console.log(error);
     res.status(401).json({ error: 'Invalid token' });
   }
 }
